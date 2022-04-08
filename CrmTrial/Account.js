b331156f02f3c9f259849c2a74869708807c929f
skerdilajdserani <skerdilajd.serani@gmail.com>
2022-04-08 15:23:16 +0200

    second commit test

---
+++ CrmTrial/Account.js
@@ -0,0 +1,47 @@
+﻿// JavaScript source code
+var CRM_FORM_TYPE_CREATE = 1;
+var CRM_FORM_TYPE_UPDATE = 2;
+var CRM_FORM_TYPE_READONLY = 3;
+var CRM_FORM_TYPE_DISABLED = 4;
+var CRM_FORM_TYPE_QUICKCREATE = 5;
+var CRM_FORM_TYPE_BULKEDIT = 6;
+
+
+//----------------------------------------------------------
+function onSaveForm(executionObj) {
+
+}
+//---------------------------------------------------
+function onLoadCreateForm() {
+
+}
+//---------------------------------------------------
+function onLoadUpdateForm() {
+    debugger;
+    formContext.getControl("fax").setDisabled(true);
+}
+//---------------------------------------------------
+
+function onLoadForm(executionContext) {
+    debugger;
+    //  alert("loaded");
+    formContext = executionContext.getFormContext();
+    GlobalContext = Xrm.Utility.getGlobalContext();
+
+    formContext.data.entity.addOnSave(onSaveForm);
+
+    formContext.getControl("fax").setDisabled(true);
+    
+    switch (formContext.ui.getFormType()) {
+        case CRM_FORM_TYPE_CREATE:
+            onLoadCreateForm();
+            break;
+        case CRM_FORM_TYPE_UPDATE:
+            onLoadUpdateForm();
+            break;
+        case CRM_FORM_TYPE_READONLY:
+            break;
+        case CRM_FORM_TYPE_QUICKCREATE:
+            break;
+    }
+}
